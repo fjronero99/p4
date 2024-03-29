060c4fd44133e38b3a20dfd66db872fecfa9c2d2
fjronero99 <70355690+fjronero99@users.noreply.github.com>
2024-03-28 19:33:24 -0500

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+#include "Utils.h"
 
+using namespace std;
+
+class stores
+{
+private:
+    int number;
+
+public:
+};
+
+void menu()
+{
+    int choice;
+    cin >> choice;
+
+    if (choice == 1)
+    {
+    }
+    else if (choice == 2)
+    {
+    }
+    else if (choice == 3)
+    {
+    }
+    else if (choice == 4)
+    {
+    }
+    else
+    {
+        cout << "GOODBYE";
+    }
+}
+
+int main()
+{
+    cout << "Select a menu option: " << endl;
+    cout << "   1. Display general grocery stores information" << endl;
+    cout << "   2. Display neighborhood charts" << endl;
+    cout << "   3. Find closest stores" << endl;
+    cout << "   4. Search for store by name" << endl;
+    cout << "   5. Exit" << endl;
+    cout << "Your choice: ";
+
+    menu();
+}
